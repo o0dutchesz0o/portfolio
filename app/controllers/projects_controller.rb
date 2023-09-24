55b6a8fa8bf7fa1b2c3f749a349873423db89764
o0dutchesz0o <80021739+o0dutchesz0o@users.noreply.github.com>
2023-09-24 16:33:29 -0500

    Update Project edit and update actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[ show edit update destroy ]
+
   def index
     @projects = Project.all
   end
@@ -12,7 +14,7 @@ class ProjectsController < ApplicationController
 
     respond_to do |format|
       if @project.save
-        format.html { redirect_to projects_path, notice: "Project was successfully created." }
+        format.html { redirect_to projects_path, notice: "#{@project.title} was successfully created." }
         format.json { render :show, status: :created, location: @project }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -20,9 +22,28 @@ class ProjectsController < ApplicationController
       end
     end
   end
+
+def edit
+end
+
+def update
+  respond_to do |format|
+    if @project.update(project_params)
+      format.html { redirect_to projects_path, notice: "#{@project.title} was successfully updated." }
+      format.json { render :show, status: :ok, location: @project }
+    else
+      format.html { render :edit, status: :unprocessable_entity }
+      format.json { render json: @project.errors, status: :unprocessable_entity }
+    end
+  end
 end
 
 private
-def project_params
-  params.require(:project).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+  def set_project
+    @project = Project.find(params[:id])
+  end
+  def project_params
+    params.require(:project).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+  end
+
 end
