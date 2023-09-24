1ede24e9ba6405d61388aa6bb0dad935c94e437f
o0dutchesz0o <80021739+o0dutchesz0o@users.noreply.github.com>
2023-09-24 16:21:35 -0500

    Created index, new and create actions and new and index views for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,28 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to projects_path, notice: "Project was successfully created." }
+        format.json { render :show, status: :created, location: @project }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+end
+
+private
+def project_params
+  params.require(:project).permit(:title, :subtitle, :body, :main_image, :thumb_image)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  resources :projects
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
   root "pages#home"
 
   resources :blogs
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :projects
 end
