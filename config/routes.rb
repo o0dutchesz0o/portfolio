70356e2cc2a88d11e04bf0f8fa547322cd0ac18e
o0dutchesz0o <80021739+o0dutchesz0o@users.noreply.github.com>
2023-09-24 14:59:43 -0500

    Add pages routes and update pages#home to be root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  root "pages#home"
+
   resources :blogs
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
