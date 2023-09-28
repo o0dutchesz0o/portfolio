b10ca87805a9d5b74809aab32c9b7f777a82d6fa
o0dutchesz0o <80021739+o0dutchesz0o@users.noreply.github.com>
2023-09-27 19:40:54 -0500

    Implemented image defaults

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,17 @@
 class Project < ApplicationRecord
+  after_initialize :set_defaults
   validates_presence_of :title, :body, :main_image, :thumb_image
 
-  # Custom scopes
-  score :sub, -> { where(subtitle: 'subtitle') }
+  # Custom scopes TODO: CAN REMOVE
+  scope :sub, -> { where(subtitle: 'subtitle') }
+
+  def set_defaults
+    self.main_image ||= "https://dummyimage.com/600x400/e319e3/208fd4.png"
+    self.thumb_image ||= "https://dummyimage.com/300x200/e319e3/208fd4.png"
+
+  end
+
+  # Custom scopes TODO: CAN REMOVE
   def self.rails
     where(subtitle: 'rails')
   end
