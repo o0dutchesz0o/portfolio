745a32dc9dbe40010ea9fdc22f618607013c61f8
Jenn Partida <dutchesz@hotmail.com>
2023-09-28 17:55:11 -0500

    Customized registration, login, and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
 
   # Defines the root path route ("/")
   root "pages#home"
