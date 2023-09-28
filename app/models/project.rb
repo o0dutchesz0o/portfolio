0bc2b1ddc270e93fc4a8cc549c7a9a801b64684f
o0dutchesz0o <80021739+o0dutchesz0o@users.noreply.github.com>
2023-09-27 19:32:55 -0500

    Add custom scope examples

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,9 @@
 class Project < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  # Custom scopes
+  score :sub, -> { where(subtitle: 'subtitle') }
+  def self.rails
+    where(subtitle: 'rails')
+  end
 end
