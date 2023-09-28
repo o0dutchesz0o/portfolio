6825896694b8843d4c776a856067e54f7f4da552
Jenn Partida <dutchesz@hotmail.com>
2023-09-28 17:54:47 -0500

    Use includes instead of .all for querying Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
 
   def index
-    @projects = Project.all
+    @projects = Project.includes(:technologies)
   end
 
   def show
