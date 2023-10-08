a47ee2db431c92fbaeef4c86b47bc55fa40efa41
o0dutchesz0o <80021739+o0dutchesz0o@users.noreply.github.com>
2023-10-07 20:27:46 -0500

    Add custom layouts for blogs and projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
-
+  layout "project"
   def index
     @projects = Project.includes(:technologies)
   end
