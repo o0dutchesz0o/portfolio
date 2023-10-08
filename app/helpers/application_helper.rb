a2cb7146aa0cf1696bda5a2ed182f63e3b0975d5
o0dutchesz0o <80021739+o0dutchesz0o@users.noreply.github.com>
2023-10-07 21:49:56 -0500

    Implement view helper for auth links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def login_helper
+     if current_user.is_a?(User)
+       link_to 'Logout', destroy_user_session_path, data: { "turbo-method": :delete } # <!-- Rails 7+ -->
+     else
+       (link_to 'Login', user_session_path) +
+         "<br>".html_safe +
+       (link_to 'Register', new_user_registration_path)
+     end
+  end
 end
